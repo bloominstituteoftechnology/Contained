1d503f60518b62c28c490e39a0f27fc19fde9423
Sean Doyle <sdoylelambda@gmail.com>
2018-11-07 18:11:49 -0500

    animation working

--- Contained - Dancing Crabs/GameViewController.swift
+++ Contained - Dancing Crabs/GameViewController.swift
@@ -1,5 +1,15 @@
 import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
     
+    @IBOutlet weak var skView: SKView!
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skView.presentScene(skscene)
+    }
 }
---
+++ Contained - Dancing Crabs/SettingsViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class SettingsViewController: UIViewController {
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+    }
+}
