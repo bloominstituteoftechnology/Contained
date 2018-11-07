aed555930af5cec9cd2c7781daf42ef240a6507f
jkaunert <jkaunert@clusterfly.net>
2018-11-07 17:35:04 -0600

    builds game screen and implements game logic

--- Contained/Contained/GameViewController.swift
+++ Contained/Contained/GameViewController.swift
@@ -1,7 +1,17 @@
 import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
 
     
-       
+    @IBOutlet weak var skview: SKView!
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+    }
+    
 }
