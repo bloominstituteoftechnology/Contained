2497c788ad494ae6cf731df3f060c568c8a93d17
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-07 16:37:04 -0700

    @jasonmodisett17
    Finished product

--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -3,12 +3,29 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     @IBAction func toggleRoll(_ sender: UISwitch) {
-        
+        if rollSwitch.isOn {
+            Model.shared.shouldRoll = true
+        } else if !rollSwitch.isOn {
+            Model.shared.shouldRoll = false
+        }
+                
     }
     
     @IBAction func toggleZoom(_ sender: UISwitch) {
+        if zoomSwitch.isOn {
+            Model.shared.sholdZoom = true
+        } else if !zoomSwitch.isOn {
+            Model.shared.sholdZoom = false
+        }
         
     }
     
+    @IBOutlet weak var rollSwitch: UISwitch!
+    
+    @IBOutlet weak var zoomSwitch: UISwitch!
+    
+    
     
 }
+
+// Create IB outlets when you need to access a property = .isOn
