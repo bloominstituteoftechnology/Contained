f65217896342f33fd27d639df126482904598d89
Frulwinn <frulwinn@gmail.com>
2018-11-07 15:45:54 -0800

    fixed switches

--- Contained/Model.swift
+++ Contained/Model.swift
@@ -4,7 +4,9 @@ class Model {
     static let shared = Model()
     private init() {}
     
+    //var for switch roll
     var shouldRoll = false
+    //var for switch zoom
     var shouldZoom = false
     
 }
--- Contained/SettingsViewController.swift
+++ Contained/SettingsViewController.swift
@@ -1,29 +1,23 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-    
-    //setting a variable for the action toggles to start with
-    var shouldToggle = false
 
     @IBAction func toggleRoll(_ sender: UISwitch) {
-        shouldToggle = sender.isOn
-        Model.shared.shouldRoll = true
-        Model.shared.shouldZoom = false
+        if sender.isOn {
+            Model.shared.shouldRoll = true
+        }else if !sender.isOn {
+            Model.shared.shouldRoll = false
+        }
     }
-        //trying to figure out how to set up a if else statement to allow if roll is chosen and if roll and zoom are chosen
-        /*
-         if Model.shared.shouldRoll = true{
-            Model.shared. shouldZoom = false
-         
-         }else{
-            
     
-        */
         
     @IBAction func toggleZoom(_ sender: UISwitch) {
-        shouldToggle = sender.isOn
-        Model.shared.shouldZoom = true
-        Model.shared.shouldRoll = false
+        if sender.isOn {
+            Model.shared.shouldZoom = true
+        }else if !sender.isOn {
+            Model.shared.shouldZoom = false
+        }
+        
     }
     
 }
