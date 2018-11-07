3c04ccddd05119cade6138c490d395893277f57e
Frulwinn <frulwinn@gmail.com>
2018-11-07 15:17:30 -0800

    added customscene and spriteview

--- Contained/SettingsViewController.swift
+++ Contained/SettingsViewController.swift
@@ -2,4 +2,28 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    //setting a variable for the action toggles to start with
+    var shouldToggle = false
+
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        shouldToggle = sender.isOn
+        Model.shared.shouldRoll = true
+        Model.shared.shouldZoom = false
+    }
+        //trying to figure out how to set up a if else statement to allow if roll is chosen and if roll and zoom are chosen
+        /*
+         if Model.shared.shouldRoll = true{
+            Model.shared. shouldZoom = false
+         
+         }else{
+            
+    
+        */
+        
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        shouldToggle = sender.isOn
+        Model.shared.shouldZoom = true
+        Model.shared.shouldRoll = false
+    }
+    
 }
