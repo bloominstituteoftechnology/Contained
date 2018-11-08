48bc526533d6cd9d46cab4621989d2ebd0831104
Stuart Sippel <stuartsippel@icloud.com>
2018-11-07 19:40:54 -0600

    Add complete xcode proj

--- Contained/Contained/InfoViewController.swift
+++ Contained/Contained/InfoViewController.swift
@@ -3,5 +3,6 @@ import UIKit
 class InfoViewController: UIViewController {
     
     @IBAction func doneButtonTapped(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
     }
 }
--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -3,9 +3,19 @@ import UIKit
 class SettingsViewController: UIViewController {
     
     @IBAction func toggleRoll(_ sender: UISwitch) {
+        if sender.isOn {
+            Model.shared.shouldRoll = true
+        } else if !sender.isOn {
+            Model.shared.shouldRoll = false
+        }
     }
     
     
     @IBAction func toggleZoom(_ sender: UISwitch) {
+        if sender.isOn {
+            Model.shared.shouldZoom = true
+        } else if !sender.isOn {
+            Model.shared.shouldZoom = false
+        }
     }
 }
