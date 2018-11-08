62c041ac3b0e98898c9e8c7d48a60cba2c7326c4
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-11-07 17:51:15 -0800

    build out info view controller sequence by adding instructions for the user

--- Contained/Contained/InfoViewController.swift
+++ Contained/Contained/InfoViewController.swift
@@ -2,4 +2,8 @@ import UIKit
 
 internal class InfoViewController : UIViewController {
     
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+        // return to the root view controller
+    }
 }
