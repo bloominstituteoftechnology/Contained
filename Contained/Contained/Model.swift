94913845bc71e0fe74f86a125eed709661cd1bc2
Sameera Leola <sameeraleola@gmail.com>
2018-11-07 19:14:39 -0600

    First commit after completing the project interface

---
+++ Contained/Contained/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  Contained
+//
+//  Created by Sameera Leola on 11/7/18.
+//  Copyright © 2018 Sameera Leola. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    static let shared = Model()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+    
+}
---
+++ Contained/Contained/SettingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewController.swift
+//  Contained
+//
+//  Created by Sameera Leola on 11/7/18.
+//  Copyright © 2018 Sameera Leola. All rights reserved.
+//
+
+import Foundation
