feb1780f2c02a809927b603cb223e682fc1b1f53
Stuart Sippel <stuartsippel@icloud.com>
2018-11-07 19:04:06 -0600

    Add SpriteKit View

--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -2,4 +2,10 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+    }
+    
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+    }
 }
