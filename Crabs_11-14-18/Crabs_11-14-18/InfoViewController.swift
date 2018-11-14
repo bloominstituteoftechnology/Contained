105f60bb9724c77827d91734041fa65807107394
Rob Herold <rderek@bresnan.net>
2018-11-14 16:26:08 -0700

    initial commit

---
+++ Crabs_11-14-18/Crabs_11-14-18/InfoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  InfoViewController.swift
+//  Crabs_11-14-18
+//
+//  Created by Rob Herold on 11/14/18.
+//  Copyright © 2018 Rob Herold. All rights reserved.
+//
+
+import Foundation
---
+++ Crabs_11-14-18/Crabs_11-14-18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Crabs_11-14-18
+//
+//  Created by Rob Herold on 11/14/18.
+//  Copyright © 2018 Rob Herold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
