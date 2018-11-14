d7e44638c88b813cd1c20816c16f803c84c5f37b
Sean Doyle <sdoylelambda@gmail.com>
2018-11-14 18:17:55 -0500

    settings storyboard and IBActions

--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -2,4 +2,13 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        
+    }
+    
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        
+    }
 }
