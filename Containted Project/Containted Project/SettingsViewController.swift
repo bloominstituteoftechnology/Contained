556a8fee1292b6a630df36fab0bbc055aac337e6
Meera Andersen <meera.andersen@gmail.com>
2018-11-14 14:56:37 -0700

    add info, play, and settings views and corresponding functionality....Forgot to commit during project.

---
+++ Containted Project/Containted Project/SettingsViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+import SpriteKit
+
+class SettingsViewController: UIViewController {
+    
+ 
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        Model.shared.shouldRoll = sender.isOn
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        Model.shared.shouldZoom = sender.isOn
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
+
