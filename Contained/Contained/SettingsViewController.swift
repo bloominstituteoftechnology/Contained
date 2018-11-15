73aaa921079e39c2bd62d5b1d27fa63f883dd6d7
Sean Doyle <sdoylelambda@gmail.com>
2018-11-14 20:53:56 -0500

    switches for zoom and roll working

--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -4,15 +4,18 @@ class SettingsViewController: UIViewController {
     
     
     @IBAction func toggleRoll(_ sender: UISwitch) {
-        if sender.isOn == true {
-         Model.shared.shouldRoll
-        } //else { return }
+        if sender.isOn {
+         Model.shared.shouldRoll = true
+        } else if !sender.isOn {
+            Model.shared.shouldRoll  = false
+        }
     }
     
-    
     @IBAction func toggleZoom(_ sender: UISwitch) {
-        if sender.isOn == true {
-            Model.shared.shouldZoom
+        if sender.isOn {
+            Model.shared.shouldZoom = true
+        } else if !sender.isOn {
+            Model.shared.shouldZoom = true
         }
     }
 }
