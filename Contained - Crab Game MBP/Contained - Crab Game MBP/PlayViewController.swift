b125b1ee0e33b15580642462352a97b57af3c2d3
Audrey Welch <welch.audreyl@gmail.com>
2018-11-14 21:02:09 -0500

    Built the game screen

--- Contained - Crab Game MBP/Contained - Crab Game MBP/PlayViewController.swift
+++ Contained - Crab Game MBP/Contained - Crab Game MBP/PlayViewController.swift
@@ -2,12 +2,20 @@
 //  PlayViewController.swift
 //  Contained - Crab Game MBP
 //
-//  Created by Audrey Seversen on 11/14/18.
-//  Copyright © 2018 Audrey Welch. All rights reserved.
-//
 
 import UIKit
+import SpriteKit
 
 class PlayViewController: UIViewController {
     
+    @IBOutlet weak var skview: SKView!
+    
+    // Create an instance of skscene that is a part of the CustomScene class
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+    }
 }
