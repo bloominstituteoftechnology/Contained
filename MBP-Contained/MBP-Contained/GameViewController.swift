79641e81a1a8f65f1575a5b5740e5ae47738266f
Vijay Das <vijaytdas@gmail.com>
2018-11-14 18:03:59 -0600

    MVP complete

--- MBP-Contained/MBP-Contained/GameViewController.swift
+++ MBP-Contained/MBP-Contained/GameViewController.swift
@@ -1,7 +1,23 @@
 import Foundation
 import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
+ 
+    
+    @IBOutlet weak var skview: SKView!
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+    }
+    
+    
+    
+    
     
     
     
--- MBP-Contained/MBP-Contained/SettingsViewController.swift
+++ MBP-Contained/MBP-Contained/SettingsViewController.swift
@@ -3,14 +3,15 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    
     @IBAction func toggleRoll(_ sender: UISwitch) {
         Model.shared.shouldRoll = sender.isOn
-        
     }
     
+   
     @IBAction func toggleZoom(_ sender: UISwitch) {
         Model.shared.shouldZoom = sender.isOn
-        
+             
     }
     
 }
