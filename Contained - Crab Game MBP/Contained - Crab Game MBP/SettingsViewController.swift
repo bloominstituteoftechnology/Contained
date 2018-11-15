4e2dfa259ecf14d4460517b842b1e1cd77c661b2
Audrey Welch <welch.audreyl@gmail.com>
2018-11-14 20:40:08 -0500

    Built the settings screen

--- Contained - Crab Game MBP/Contained - Crab Game MBP/SettingsViewController.swift
+++ Contained - Crab Game MBP/Contained - Crab Game MBP/SettingsViewController.swift
@@ -2,12 +2,23 @@
 //  SettingsViewController.swift
 //  Contained - Crab Game MBP
 //
-//  Created by Audrey Seversen on 11/14/18.
-//  Copyright © 2018 Audrey Welch. All rights reserved.
-//
+
 
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+       
+        // When toggle is on, initiate the shouldRoll property in the model
+        Model.shared.shouldRoll = sender.isOn
+        
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        
+        // When toggle is on, initiate the shouldZoom property in the model
+        Model.shared.shouldZoom = sender.isOn
+    }
     
 }
