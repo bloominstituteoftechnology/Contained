7b30121b80f6018bad3a5b3c64c7ee79d63afab6
Luis Corvalan <LRCorvalan@gmail.com>
2019-01-09 15:22:50 -0600

    Start building Settings Model

---
+++ Contained/Contained/Settings.swift
@@ -0,0 +1,16 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Luis Corvalan on 1/9/19.
+//  Copyright © 2019 Luis Corvalan. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    
+    var shouldRoll = false
+    var shouldZoom = false
+    
+}
