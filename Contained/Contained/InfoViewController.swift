5d233bb04d2959ea95c790fd484c49cb069dee3e
Stuart Sippel <stuartsippel@icloud.com>
2019-01-09 15:12:15 -0600

    Build info screen

--- Contained/Contained/InfoViewController.swift
+++ Contained/Contained/InfoViewController.swift
@@ -10,4 +10,7 @@ import UIKit
 
 class InfoViewController: UIViewController {
     
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
 }
