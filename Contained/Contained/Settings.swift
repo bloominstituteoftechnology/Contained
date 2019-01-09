976405ebc2149595f37a2e57b6dff98b03bad7ba
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-09 12:34:04 -0800

    Build Settings model and add assets to project.

---
+++ Contained/Contained/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Nathanael Youngren on 1/9/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    var shouldRoll = false
+    var shouldZoom = false
+}
