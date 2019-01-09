543b593727be856cd2bead40812646d3c1bc380a
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-09 13:20:42 -0800

    Build the Info sequence. Including instructions and functionality.

--- Contained/Contained/InfoViewController.swift
+++ Contained/Contained/InfoViewController.swift
@@ -12,8 +12,12 @@ class InfoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
+    
 }
