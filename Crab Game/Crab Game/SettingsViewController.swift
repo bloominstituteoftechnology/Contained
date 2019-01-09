00aff166de3f8d971f41f0fbc82a18e6d700a9b3
Dustin Koch <dustin.koch@me.com>
2019-01-09 17:23:14 -0600

    Able to build app with these changes to make Crab move but not spin/zoom: Remove Model code (errors) and replace with initiated settings

--- Crab Game/Crab Game/SettingsViewController.swift
+++ Crab Game/Crab Game/SettingsViewController.swift
@@ -9,22 +9,21 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    let setting = Settings()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        setting.shouldZoom = sender.isOn
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        setting.shouldZoom = sender.isOn
     }
-    */
+    
+    
+    
+
+
 
 }
