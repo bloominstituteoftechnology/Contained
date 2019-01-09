3ddf79409e386a1e87b48e5f47975b1713aedb50
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-09 13:45:42 -0800

    Build Settings screen with roll and zoom switch functionality.

--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -12,8 +12,23 @@ class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        if sender.isOn {
+            settings.shouldRoll = true
+        } else {
+            settings.shouldRoll = false
+        }
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        if sender.isOn {
+            settings.shouldZoom = true
+        } else {
+            settings.shouldZoom = false
+        }
+    }
+    
+    let settings = Settings()
 }
