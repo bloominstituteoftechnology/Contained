adfc6c3a3c1ff4774bececc29bf04f6c34446589
Vijay Das <vijaytdas@gmail.com>
2019-01-09 14:14:57 -0600

    add layout for Settings screen, add outlets to SettingsViewController for Roll and Zoom toggle switches

--- iOS5-VTD-Contained/iOS5-VTD-Contained/SettingsViewController.swift
+++ iOS5-VTD-Contained/iOS5-VTD-Contained/SettingsViewController.swift
@@ -16,15 +16,13 @@ class SettingsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    @IBAction func shouldRoll(_ sender: UISwitch) {
     }
-    */
-
+    
+    
+    
+    @IBAction func shouldZoom(_ sender: UISwitch) {
+    }
+    
 }
