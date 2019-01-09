232b01b5fe5d19f84dbf293d050aa382bde83199
Paul Yi <yipaulc@gmail.com>
2019-01-09 14:42:44 -0800

    Build the Info sequence

--- Contained/Contained/InfoViewController.swift
+++ Contained/Contained/InfoViewController.swift
@@ -2,4 +2,8 @@ import UIKit
 
 class InfoViewController: UIViewController {
     
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
 }
