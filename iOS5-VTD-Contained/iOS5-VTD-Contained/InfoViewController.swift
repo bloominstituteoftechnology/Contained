00c18a9740ac63b4511d60c5882114e2ea82c5af
Vijay Das <vijaytdas@gmail.com>
2019-01-09 13:56:24 -0600

    add SpriteKit-Utility file, add three view controller, add settings.swift

---
+++ iOS5-VTD-Contained/iOS5-VTD-Contained/InfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoViewController.swift
+//  iOS5-VTD-Contained
+//
+//  Created by Vijay Das on 1/9/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS5-VTD-Contained/iOS5-VTD-Contained/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  iOS5-VTD-Contained
+//
+//  Created by Vijay Das on 1/9/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    var shouldRoll = false
+    var shouldZoom = false
+}
