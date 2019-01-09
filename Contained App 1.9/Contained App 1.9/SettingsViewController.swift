7a24f4c321b3d687f63274c96f81eee76ef2be6d
Sean Doyle <sdoylelambda@gmail.com>
2019-01-09 16:00:42 -0500

    Settings storyboard completed.

--- Contained App 1.9/Contained App 1.9/SettingsViewController.swift
+++ Contained App 1.9/Contained App 1.9/SettingsViewController.swift
@@ -2,5 +2,10 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+     //   Settings.shouldRoll.toggle
+    }
+    
+    @IBAction func ToggleZoom(_ sender: UISwitch) {
+    }
 }
