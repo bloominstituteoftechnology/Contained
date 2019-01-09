8a1120ed6057af5d6befaa5fc4ca836c8b2b3948
Jocelyn Stuart <luluscomputer@jocelyns-macbook-air.local>
2019-01-09 18:13:19 -0500

    Will allow user to tap the tab bar to navigate to settings, game, and info. Will roll crab when roll is switched on, and zoom in crab when zoom is switched on, and both at the same time. Will navigate through info pages with navigation bar.

---
+++ Contained/Contained/InfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  InfoViewController.swift
+//  Contained
+//
+//  Created by Jocelyn Stuart on 1/9/19.
+//  Copyright © 2019 JS. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+}
---
+++ Contained/Contained/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  Contained
+//
+//  Created by Jocelyn Stuart on 1/9/19.
+//  Copyright © 2019 JS. All rights reserved.
+//
+
+import UIKit
+
+
