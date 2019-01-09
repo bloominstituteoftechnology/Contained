52b14556fabb5ead134cfedf13dc8ae76d58f875
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-09 10:15:37 -0500

    Create three classes in three new files: GameViewController, InfoViewController, and SettingsViewController. Each descends from UIViewController but is otherwise empty. Assign these classes to your view controllers using the IB identity inspector.

---
+++ ContainedApp/ContainedApp/View Controllers/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  ContainedApp
+//
+//  Created by Nelson Gonzalez on 1/9/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
