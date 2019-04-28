43c7d26d8b04d4bc6b17ef0f1bb8daad50270eca
Dan Morse <morsedan@outlook.com>
2019-04-28 08:18:04 -0700

    Added all views to IB. Added some text to the info series of VeiwControllers. Added GameViewController, InfoViewController, SettingsViewController. Set Done button to pop off to the front of the info series.

---
+++ Contained/Contained/InfoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  InfoViewController.swift
+//  Contained
+//
+//  Created by morse on 4/28/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Contained/Contained/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by morse on 4/28/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    var shouldRoll = false
+    var shouldZoom = false
+}
