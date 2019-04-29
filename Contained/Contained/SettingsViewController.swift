9786d1c0165e0d0e68614fe7a8cac60f5b426b37
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-04-29 18:10:42 -0400

    Fixed a bug in settings, where the crab would still roll or zoom even if the switch was turned off after being turned on.

--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -19,12 +19,16 @@ class SettingsViewController: UIViewController {
     @IBAction func toggleRoll(_ sender: UISwitch) {
         if sender.isOn == true {
             Settings.shared.shouldRoll = true
+        } else {
+            Settings.shared.shouldRoll = false
         }
     }
     
     @IBAction func toggleZoom(_ sender: UISwitch) {
         if sender.isOn == true {
             Settings.shared.shouldZoom = true
+        } else {
+            Settings.shared.shouldZoom = false
         }
     }
 }
