9863d898de7d410db7d7bae75d6feced37feead2
Christopher Aronson <christopheraronson@icloud.com>
2019-04-29 12:51:55 -0700

    Set up info view nav items

--- Contained/Contained/InfoViewController.swift
+++ Contained/Contained/InfoViewController.swift
@@ -26,5 +26,8 @@ class InfoViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
 }
