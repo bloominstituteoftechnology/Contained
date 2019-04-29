116f6ff798c6ba340ff5fe1925a95389afbf1260
Michael Redig <mredig@gmail.com>
2019-04-29 15:37:30 -0500

    completed MVP

---
+++ CrabbyControls/CrabbyControls/CustomScene.swift
@@ -0,0 +1,48 @@
+//
+//  CustomScene.swift
+//  CrabbyControls
+//
+//  Created by Michael Redig on 4/29/19.
+//  Copyright © 2019 Michael Redig. All rights reserved.
+//
+
+import SpriteKit
+
+
+class CustomScene: SKScene {
+	let crab = SKSpriteNode()
+	
+	override func sceneDidLoad() {
+		super.sceneDidLoad()
+		addChild(crab)
+		crab.loadTextures(named: "HappyCrab", forKey: SKSpriteNode.textureKey)
+		crab.position = CGPoint(x: frame.midX, y: frame.midY)
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard !touches.isEmpty, let touch = touches.first else { return }
+		
+		let position = touch.location(in: self)
+		
+		let actionDuration = 1.0
+		let moveAction = SKAction.move(to: position, duration: actionDuration)
+		
+		let rollAction = SKAction.rotate(byAngle: .pi * 2, duration: actionDuration)
+		let zoomAction = SKAction.scale(by: 1.3, duration: actionDuration * 0.3)
+		let unzoomAction = SKAction.scale(to: 1.0, duration: actionDuration * 0.1)
+		
+		if Settings.shouldZoom {
+			let zoomSeq = SKAction.sequence([zoomAction, moveAction, unzoomAction])
+			crab.run(zoomSeq)
+		} else {
+			crab.run(moveAction)
+		}
+		
+		if Settings.shouldRoll {
+			crab.run(rollAction)
+		}
+		
+	}
+	
+	
+}
--- CrabbyControls/CrabbyControls/GameViewController.swift
+++ CrabbyControls/CrabbyControls/GameViewController.swift
@@ -7,24 +7,22 @@
 //
 
 import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet var gameView: SKView!
+	
+	var skscene: CustomScene? = nil
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		skscene = CustomScene(size: view.bounds.size)
+		gameView.presentScene(skscene)
+	}
 }
