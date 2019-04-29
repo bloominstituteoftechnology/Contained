e5f5981f7180ee2b7914bb3c743d872fa02d6d57
Mitchell Budge <mitchellbudge@gmail.com>
2019-04-29 15:20:12 -0600

    Added Sprite Kit to the Game VC and input code to create the game interface. Created a CustomScene.swift file to input code for game functionality.

--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -16,9 +16,14 @@ class SettingsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        Settings.shared.shouldRoll = !Settings.shared.shouldRoll
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        Settings.shared.shouldZoom = !Settings.shared.shouldZoom
+    }
     /*
-    // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
