3c439ef9923f571cc99bcd101af6f1015fdf04d7
Mitchell Budge <mitchellbudge@gmail.com>
2019-04-29 13:56:08 -0600

    Added a Settings.swift file with code configurations for shouldRoll and shouldZoom

---
+++ Contained/Contained/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Mitchell Budge on 4/29/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    var shouldRoll = false
+    var shouldZoom = false
+}
