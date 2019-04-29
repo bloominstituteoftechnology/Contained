d02b7ba4d7e2b711c4e76c08505b73cd7f6f0f52
Sameera Leola <pixelandbyte@gmail.com>
2019-04-29 17:50:33 -0500

    Setting up Game controller. Having trouble adding SKView outlet

--- Contained/Contained/Controllers/GameViewController.swift
+++ Contained/Contained/Controllers/GameViewController.swift
@@ -7,10 +7,20 @@
 //
 
 import UIKit
+import SpriteKit
 
-class GameViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class GamesViewController: UIViewController {
+    
+    
+    @IBOutlet weak var skview: SKView!
+    
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
     }
     
     @IBAction func unwindToPlay(_ sender: UIStoryboardSegue) {
