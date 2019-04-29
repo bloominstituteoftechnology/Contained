db80ea3be467775d74330df5f194a4036b44274a
Christopher Aronson <christopheraronson@icloud.com>
2019-04-29 12:21:31 -0700

    Added assets and Settings Class

---
+++ Contained/Contained/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Christopher Aronson on 4/29/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    var shouldRoll = false
+    var shouldZoom = false
+}
