74cee4dd1f15db5a02bad0683b390a8dc768917b
Mitchell Budge <mitchellbudge@gmail.com>
2019-04-29 14:04:12 -0600

    Built the interface with three tabs: info, play, and settings. The info tab controller was embedded in a navigation controller, and two additional view controllers were connected in sequence to the info tab controller.

---
+++ Contained/Contained/InfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoViewController.swift
+//  Contained
+//
+//  Created by Mitchell Budge on 4/29/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
