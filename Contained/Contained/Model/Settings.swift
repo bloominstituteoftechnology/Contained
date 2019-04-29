9475b4c9e21f83d0da2582d1770226cafadd463e
Sameera Leola <pixelandbyte@gmail.com>
2019-04-29 15:56:30 -0500

    Finished setting up the storyboards and adding the sprite control file.

---
+++ Contained/Contained/Model/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Sameera Roussi on 4/29/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    var shouldRoll = false
+    var shouldZoom = false
+}
