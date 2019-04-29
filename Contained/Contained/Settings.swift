dadf3d8c714c1fc13ccdf5c81ffb4742b7c7486b
Jeffrey Carpenter <spaz926@me.com>
2019-04-29 14:09:31 -0500

    Added the Settings Model

---
+++ Contained/Contained/Settings.swift
@@ -0,0 +1,16 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Jeffrey Carpenter on 4/29/19.
+//  Copyright © 2019 Jeffrey Carpenter. All rights reserved.
+//
+
+import Foundation
+
+struct Settings {
+    
+    var shouldRoll = false
+    var shouldZoom = false
+    
+}
