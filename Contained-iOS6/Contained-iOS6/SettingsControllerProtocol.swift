4b6256e4bf57b082d5655e0d5fa5fc038a05e38f
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-29 14:30:43 -0700

    settingsControllerProtocol

---
+++ Contained-iOS6/Contained-iOS6/SettingsControllerProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsControllerProtocol.swift
+//  Contained-iOS6
+//
+//  Created by Hector Steven on 4/29/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Foundation
+
+protocol SettingsControllerProtocol {
+	var setingsControllerProtocol: SettingsController? { set get }
+}
