8e345875c04a71f9386cc4481a51110849f1b524
Michael Redig <mredig@gmail.com>
2019-04-29 17:00:47 -0500

    all stretch goals, but modified

--- CrabbyControls/CrabbyControls/CustomScene.swift
+++ CrabbyControls/CrabbyControls/CustomScene.swift
@@ -12,6 +12,8 @@ import SpriteKit
 class CustomScene: SKScene {
 	let crab = SKSpriteNode()
 	
+	var heartAttacking = false
+	
 	override func sceneDidLoad() {
 		super.sceneDidLoad()
 		addChild(crab)
@@ -22,34 +24,75 @@ class CustomScene: SKScene {
 	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 		guard !touches.isEmpty, let touch = touches.first else { return }
 		
+		guard !heartAttacking else { return }
+		
 		let position = touch.location(in: self)
 		Settings.lastTouch = position
-		
 		let actionDuration = 1.0
-		let moveAction = SKAction.move(to: position, duration: actionDuration)
+
 		
-		let rollAction = SKAction.rotate(byAngle: .pi * 2, duration: actionDuration)
-		let zoomAction = SKAction.scale(by: 1.3, duration: actionDuration * 0.3)
-		let unzoomAction = SKAction.scale(to: 1.0, duration: actionDuration * 0.1)
-		
-		let resumeWaiting = SKAction.customAction(withDuration: 0.01) { (node, value) in
-			self.crab.loadTextures(named: "WaitingCrab", forKey: SKSpriteNode.textureKey)
-		}
-		
-		crab.loadTextures(named: "HappyCrab", forKey: SKSpriteNode.textureKey)
-		
-		if Settings.shouldZoom {
-			let zoomSeq = SKAction.sequence([zoomAction, moveAction, unzoomAction, resumeWaiting])
-			crab.run(zoomSeq)
+		if Settings.heartAttackMode {
+			heartAttacking = true
+			
+			let moveAction = SKAction.move(to: position, duration: actionDuration)
+			
+			let scaleAction = SKAction.scale(to: 0.1, duration: actionDuration * 2)
+			let waitAction = SKAction.wait(forDuration: actionDuration * 3)
+			let disappearAction = SKAction.scale(to: 0, duration: actionDuration * 0.07)
+			let waitAgainAction = SKAction.wait(forDuration: actionDuration * 0.3)
+			let scareAction = SKAction.customAction(withDuration: 0.01) { [weak self] (node, value) in
+				guard let self = self else {print("no self"); return }
+				let scareSprite = SKSpriteNode(imageNamed: "scaryCrab")
+				scareSprite.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+				scareSprite.zPosition = 1
+				scareSprite.setScale(0)
+				self.addChild(scareSprite)
+				
+				let wiggle = SKAction.sequence([SKAction.rotate(toAngle: 3 * (.pi / 180), duration: 0.1), SKAction.rotate(toAngle: -3 * (.pi / 180), duration: 0.1), ])
+				let wiggleForever = SKAction.repeatForever(wiggle)
+				let zoom = SKAction.scale(to: 1.0, duration: 0.05)
+				let scareNoise = SKAction.playSoundFileNamed("scream.mp3", waitForCompletion: false)
+				let scareGroup = SKAction.group([wiggleForever, zoom, scareNoise])
+				scareSprite.run(scareGroup)
+				let resetScene = SKAction.customAction(withDuration: 0.01, actionBlock: { [weak self] (node, value) in
+					self?.crab.setScale(1)
+					self?.heartAttacking = false
+				})
+				let removeScare = SKAction.sequence([SKAction.wait(forDuration: 5), SKAction.removeFromParent(), resetScene])
+				scareSprite.run(removeScare)
+			}
+			
+			let moveSeq = SKAction.sequence([SKAction.group([moveAction, scaleAction]), waitAction, disappearAction, waitAgainAction, scareAction])
+			crab.run(moveSeq)
+			
+			
+			
+			
+			
 		} else {
-			crab.run(SKAction.sequence([moveAction, resumeWaiting]))
+			//regular mode
+			let moveAction = SKAction.move(to: position, duration: actionDuration)
+			
+			let rollAction = SKAction.rotate(byAngle: .pi * 2, duration: actionDuration)
+			let zoomAction = SKAction.scale(by: 1.3, duration: actionDuration * 0.3)
+			let unzoomAction = SKAction.scale(to: 1.0, duration: actionDuration * 0.1)
+			
+			let resumeWaiting = SKAction.customAction(withDuration: 0.01) { (node, value) in
+				self.crab.loadTextures(named: "WaitingCrab", forKey: SKSpriteNode.textureKey)
+			}
+			
+			crab.loadTextures(named: "HappyCrab", forKey: SKSpriteNode.textureKey)
+			
+			if Settings.shouldZoom {
+				let zoomSeq = SKAction.sequence([zoomAction, moveAction, unzoomAction, resumeWaiting])
+				crab.run(zoomSeq)
+			} else {
+				crab.run(SKAction.sequence([moveAction, resumeWaiting]))
+			}
+			
+			if Settings.shouldRoll {
+				crab.run(rollAction)
+			}
 		}
-		
-		if Settings.shouldRoll {
-			crab.run(rollAction)
-		}
-		
 	}
-	
-	
 }
