0da91cad840195fef937827fd541282e67689978
jayfenam <jonathanferrer95@gmail.com>
2019-04-29 15:23:35 -0400

    Added three storyboards and embedded them in a tab view

---
+++ CrabGame/CrabGame/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  CrabGame
+//
+//  Created by Jonathan Ferrer on 4/29/19.
+//  Copyright © 2019 Jonathan Ferrer. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    var shouldRoll = false
+    var shouldZoom = false
+}
