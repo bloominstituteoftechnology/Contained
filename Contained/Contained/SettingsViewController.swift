445e0bc903cc9cefba0291435e06be6a87d35f71
Jeffrey Carpenter <spaz926@me.com>
2019-04-29 15:54:48 -0500

    Updated Settings VC to use the switch's isOn property
    I was initially setting the shouldRoll and shouldZoom properties to the inverse of it's current value. But have updated to a safer way.

--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -24,10 +24,10 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func toggleRoll(_ sender: UISwitch) {
-        Settings.shared.shouldRoll = !Settings.shared.shouldRoll
+        Settings.shared.shouldRoll = sender.isOn
     }
     
     @IBAction func toggleZoom(_ sender: UISwitch) {
-        Settings.shared.shouldZoom = !Settings.shared.shouldZoom
+        Settings.shared.shouldZoom = sender.isOn
     }
 }
