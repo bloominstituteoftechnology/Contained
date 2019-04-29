8478f245da8265d89fb907e0a749b45def0501b6
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-29 13:39:50 -0700

    ?.viewControllers.count and swiftch case setup

--- Contained-iOS6/Contained-iOS6/StoryBoards/InfoViewController.swift
+++ Contained-iOS6/Contained-iOS6/StoryBoards/InfoViewController.swift
@@ -12,14 +12,32 @@ class InfoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-		setupLabel()
+		
     }
 	
-	func setupLabel () {
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		if let viewControllerNumber = navigationController?.viewControllers.count {
+			
+			switch viewControllerNumber {
+			case 1:
+				setupLabel1()
+			case 2:
+				setupLabel1()
+			case 3:
+				setupLabel1()
+			default:
+				print("no view")
+			}
+
+		}
+	}
+	
+	func setupLabel1 () {
 		label.text = "Crabs"
 		label.textAlignment = .center
-		label.font = UIFont.boldSystemFont(ofSize: 64)
+		label.font = UIFont.boldSystemFont(ofSize: 48)
 		label.sizeToFit()
 		label.center = view.center
 		view.addSubview(label)
