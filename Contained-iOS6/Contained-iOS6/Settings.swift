bda6ba5a718d4c7e30397900b21645bb4fd18987
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-29 12:54:18 -0700

    Setings()

---
+++ Contained-iOS6/Contained-iOS6/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  Contained-iOS6
+//
+//  Created by Hector Steven on 4/29/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+	var shouldRoll = false
+	var shouldZoom = false
+}
