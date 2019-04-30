1a461602100e4b9dd72ac7aeda52092c5021c575
Michael R.F <mrflowers22@uwalumni.com>
2019-04-30 09:22:01 -0400

    added images and created the settings.swift file.

---
+++ Contained1/Contained1/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  Contained1
+//
+//  Created by Michael Flowers on 4/30/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    var shouldRoll = false
+    var shouldZoom = false
+}
