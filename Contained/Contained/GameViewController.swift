28e95f66c1f82124da579a311b4d0c8d169de320
Austin Martin <armartin2300@gmail.com>
2019-05-06 16:09:57 -0700

    Fixed issue with duplicate line of code causing issue with view connection.

--- Contained/Contained/GameViewController.swift
+++ Contained/Contained/GameViewController.swift
@@ -9,21 +9,19 @@
 import SpriteKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+        
+    @IBOutlet weak var skview: SKView!
     
-    class GamesViewController: UIViewController {
-        @IBOutlet weak var skview: SKView!
-        
-        var skscene: CustomScene? = nil
-        
-        override func viewDidAppear(_ animated: Bool) {
-            super.viewDidAppear(animated)
-            skscene = CustomScene(size: view.bounds.size)
-            skview.presentScene(skscene)
-        }
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
     }
-
 }
