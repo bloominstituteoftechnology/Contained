186430bbbec1ad5edf518b7074c3ef5cf7397a0a
Alex Perse <alexanderperse@gmail.com>
2019-05-06 16:04:24 -0400

    Added settings UISwitches and functionality

--- NewContainedApp/NewContainedApp/SettingsViewController.swift
+++ NewContainedApp/NewContainedApp/SettingsViewController.swift
@@ -10,21 +10,32 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    
+    @IBAction func rollSwitch(_ sender: UISwitch) {
+        if (sender.isOn) {
+            Settings.shared.shouldRoll = true
+//            print("roll is on") //sanity check
+        } else {
+            Settings.shared.shouldRoll = false
+//            print("roll is off") //sanity check
+        }
+    }
+    
+    @IBAction func zoomSwitch(_ sender: UISwitch) {
+        if (sender.isOn) {
+            Settings.shared.shouldZoom = true
+        } else {
+            Settings.shared.shouldZoom = false
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
 
 }
