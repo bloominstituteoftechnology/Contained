bcab4fd31179c912861c7a5d284fcccec01cbddd
Kat Milton <mltnkat@gmail.com>
2019-06-10 13:04:53 -0700

    MVP update. Game is working and all tabs are functional.

--- Crab Game/Crab Game/View Controllers/SettingsViewController.swift
+++ Crab Game/Crab Game/View Controllers/SettingsViewController.swift
@@ -16,15 +16,17 @@ class SettingsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - IBActions
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        Settings.shared.shouldRoll.toggle()
     }
-    */
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        Settings.shared.shouldZoom.toggle()
+    }
+    
+
+    
 
 }
