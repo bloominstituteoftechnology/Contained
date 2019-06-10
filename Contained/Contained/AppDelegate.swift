87d51124e057d094629dba28b8e7e4813d1fb6a1
shillwil <alex.shillingford@icloud.com>
2019-06-10 13:37:27 -0600

    First commit

---
+++ Contained/Contained/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  Contained
+//
+//  Created by Alex Shillingford on 6/10/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+}
+
---
+++ Contained/Contained/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contained
+//
+//  Created by Alex Shillingford on 6/10/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
