5e60a61d213ad903d420f1741ecfea8dd507dcba
Marlon Raskin <marlonjraskin@gmail.com>
2019-06-10 18:13:00 -0400

    UI changes and refactored roll and zoom toggles code

--- Contained/Contained/ViewControllers/SettingsViewController.swift
+++ Contained/Contained/ViewControllers/SettingsViewController.swift
@@ -19,22 +19,10 @@ class SettingsViewController: UIViewController {
     }
     
 	@IBAction func toggleRoll(_ sender: UISwitch) {
-		if !rollSwitch.isOn {
-			rollSwitch.isOn = true
-			Settings.shared.shouldRoll = true
-		} else if rollSwitch.isOn {
-			rollSwitch.isOn = false
-			Settings.shared.shouldRoll = false
-		}
+		Settings.shared.shouldRoll = !Settings.shared.shouldRoll
 	}
 	
 	@IBAction func toggleZoom(_ sender: UISwitch) {
-		if !zoomSwitch.isOn {
-			zoomSwitch.isOn = true
-			Settings.shared.shouldZoom = true
-		} else if zoomSwitch.isOn {
-			zoomSwitch.isOn = false
-			Settings.shared.shouldZoom = false
-		}
+		Settings.shared.shouldZoom = !Settings.shared.shouldZoom
 	}
 }
