cf487d32b6cf93048cd1e8583830f9f384e50e05
Marlon Raskin <marlonjraskin@gmail.com>
2019-06-10 16:37:35 -0400

    Settings page is done. Labels are set up, toggles are working properly.

--- Contained/Contained/ViewControllers/SettingsViewController.swift
+++ Contained/Contained/ViewControllers/SettingsViewController.swift
@@ -10,13 +10,31 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet var rollSwitch: UISwitch!
+	@IBOutlet var zoomSwitch: UISwitch!
+	
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+	@IBAction func toggleRoll(_ sender: UISwitch) {
+		if !rollSwitch.isOn {
+			rollSwitch.isOn = true
+		} else if rollSwitch.isOn {
+			rollSwitch.isOn = false
+		}
+	}
+	@IBAction func toggleZoom(_ sender: UISwitch) {
+		if !zoomSwitch.isOn {
+			zoomSwitch.isOn = true
+		} else if zoomSwitch.isOn {
+			zoomSwitch.isOn = false
+		}
+	}
+	
     /*
     // MARK: - Navigation
 
