a4aa71f2e3496ddd27e89cf6146f5d01ad39ae57
Marlon Raskin <marlonjraskin@gmail.com>
2019-06-10 15:32:29 -0400

    Added 3 ViewControllers

---
+++ Contained/Contained/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Marlon Raskin on 6/10/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+	static let shared = Settings()
+	private init() {}
+	
+	var shouldRoll = false
+	var shouldZoom = false
+}
