e6fec392a9bded7737bb066e3653de6cf8e15e6b
Ife Banire <IfeBanire@yahoo.com>
2019-06-13 11:22:36 -0400

    Finished project

--- Crab/Crab/Controllers/SettingsViewController.swift
+++ Crab/Crab/Controllers/SettingsViewController.swift
@@ -15,7 +15,9 @@ class SettingsViewController: UIViewController {
     }
 
     @IBAction func toggleRoll(_ sender: UISwitch) {
+        Settings.shared.shouldRoll = sender.isOn
     }
     @IBAction func toggleZoom(_ sender: UISwitch) {
+        Settings.shared.shouldZoom = sender.isOn
     }
 }
