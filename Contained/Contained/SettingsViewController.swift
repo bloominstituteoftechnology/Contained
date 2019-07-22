d60989eb89991b8375ac9060d36fe05833735e67
JerryHaaser <jerry.haaser@hotmail.com>
2019-07-22 16:21:37 -0500

    Added Roll and Zoom switches

--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        
+    }
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
