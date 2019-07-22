5b0094d2e1bf7cf9f6c32e20dabdf84e7701a546
bfvilchez <bfvilchez@icloud.com>
2019-07-22 14:45:45 -0600

    finished setting up view controllers

---
+++ Contained/Contained/Settings.swift
@@ -0,0 +1,13 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by brian vilchez on 7/22/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import Foundation
+class Settings {
+    var shouldRoll = false
+    var shouldZoom = false
+}
---
+++ Contained/Contained/SettingsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsViewController.swift
+//  Contained
+//
+//  Created by brian vilchez on 7/22/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    var settings = Settings()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        if sender.isOn {
+            
+        }
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        if sender.isOn {
+            
+        }
+        
+    }
+    
+  
+
+}
