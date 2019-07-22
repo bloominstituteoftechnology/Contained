96d2f04164edea7d3d0780a6f12c949d6f820bb5
Taylor Lyles <hawks93187@gmail.com>
2019-07-22 13:55:28 -0700

    wired up switch in SettingsVC

--- Crab/Crab/ViewControllers/SettingsViewController.swift
+++ Crab/Crab/ViewControllers/SettingsViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    override func viewDidLoad() {
+	@IBAction func toggleRoll(_ sender: UISwitch) {
+		if sender.isOn {
+			Settings.shared.shouldRoll = true
+		}
+	}
+	
+	@IBAction func toggleZoom(_ sender: UISwitch) {
+		Settings.shared.shouldZoom = true
+	}
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+		
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
