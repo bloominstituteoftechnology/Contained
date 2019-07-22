64f1bbd0498faaf73d882aff1d6ff8f3f5ae8a62
Donovan Langager <donovan.lan16@gmail.com>
2019-07-22 14:59:31 -0700

    Added all controllers

---
+++ Crab Game/Crab Game/View Controllers/SettingsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.swift
+//  Crab Game
+//
+//  Created by Uptiie on 7/22/19.
+//  Copyright © 2019 Walcenberg, Inc. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+    }
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+    }
+}
