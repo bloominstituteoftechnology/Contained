e50ce7b4bc3424a07bd6985b0bc5704523eb1569
shillwil <alex.shillingford@icloud.com>
2019-07-22 14:57:34 -0600

    Settings Screen built with zoom and roll switches toggling properly

--- Contained/Contained/View Controllers/SettingsViewController.swift
+++ Contained/Contained/View Controllers/SettingsViewController.swift
@@ -9,22 +9,27 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    @IBOutlet weak var rollSwitch: UISwitch!
+    @IBOutlet weak var zoomSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        rollSwitch.isOn = false
+        zoomSwitch.isOn = false
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        sender.isOn.toggle()
+        print(sender.isOn)
     }
-    */
-
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        sender.isOn.toggle()
+        print(sender.isOn)
+    }
+    
 }
