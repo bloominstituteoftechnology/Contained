c1986cbe8a75877d8c3c709a444200bfd638f7f1
Blake Andrew Price <blakeandrewprice@gmail.com>
2019-07-22 14:49:30 -0700

    Completed Build the Settings Screen

--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -10,21 +10,18 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        Settings.shared.shouldRoll.toggle()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        Settings.shared.shouldZoom.toggle()
     }
-    */
-
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
