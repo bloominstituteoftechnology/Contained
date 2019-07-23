df159e9b07ecdd0021a85dcb7502297e2a6eef97
JerryHaaser <jerry.haaser@hotmail.com>
2019-07-22 19:26:14 -0500

    Corrected sKView naming that was bugging code.

--- Contained/Contained/GameViewController.swift
+++ Contained/Contained/GameViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
-    @IBOutlet weak var skview: SKView!
+    @IBOutlet weak var sKView: SKView!
     
     var skscene: CustomScene? = nil
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         skscene = CustomScene(size: view.bounds.size)
-        skview.presentScene(skscene)
+        sKView.presentScene(skscene)
     }
     
     override func viewDidLoad() {
--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SpriteKit
 
 class SettingsViewController: UIViewController {
 
