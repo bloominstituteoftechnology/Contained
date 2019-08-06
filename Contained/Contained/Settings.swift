94ab8e0725657d1794ffe18d65babad70199ff94
John Kouris <jdkouris@gmail.com>
2019-08-05 19:33:16 -0700

    Built the settings model

---
+++ Contained/Contained/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by John Kouris on 8/5/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+}
