dc11b9e5defc7766a9ae68a6548d4c9f0f1f8343
John Kouris <jdkouris@gmail.com>
2019-08-05 19:26:02 -0700

    Created the skeleton project

---
+++ Contained/Contained/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contained
+//
+//  Created by John Kouris on 8/5/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
