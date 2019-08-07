34e8ec71396611c6f6513e3bbdb378bb5fbcbf7b
Eoin Lavery <dev@tenantc.co.uk>
2019-08-07 18:08:15 +0100

    Completed Info Sequence

--- Contained/Contained/View Controllers/InfoViewController.swift
+++ Contained/Contained/View Controllers/InfoViewController.swift
@@ -10,21 +10,8 @@ import UIKit
 
 class InfoViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
