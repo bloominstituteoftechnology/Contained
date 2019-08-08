25c4411168c3ae5eb09c5ad90ed7489cfedc1b34
JosephRogers94 <Joerogersss2016@gmail.com>
2019-08-07 22:22:31 -0500

    Sprite Kit Add

--- Crabs/Crabs/VC/GameViewController.swift
+++ Crabs/Crabs/VC/GameViewController.swift
@@ -5,26 +5,16 @@
 //  Created by Joseph Rogers on 8/7/19.
 //  Copyright © 2019 Joe Rogers. All rights reserved.
 //
-
 import UIKit
-
-class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+import SpriteKit
+class GamesViewController: UIViewController {
+    @IBOutlet weak var skview: SKView!
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
     }
-    */
-
 }
