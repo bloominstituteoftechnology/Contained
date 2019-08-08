3ed5abd47984f86937caf1f9f0f8f59b879a842a
Jessie Griffin <i.jessie.me@icloud.com>
2019-08-07 21:57:20 -0400

    "Build the Game Screen" complete

--- Contained/Contained/View Controllers/GameViewController.swift
+++ Contained/Contained/View Controllers/GameViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var skview: SKView!
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+    }
+    
+        
+        
 
         // Do any additional setup after loading the view.
     }
