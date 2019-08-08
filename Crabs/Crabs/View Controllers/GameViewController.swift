37e53b5ce96d0590b8a74907e5cf3e4f2a8bc828
Joel Groomer <joelgroomer@gmail.com>
2019-08-07 18:29:55 -0700

    MVP achieved.

--- Crabs/Crabs/View Controllers/GameViewController.swift
+++ Crabs/Crabs/View Controllers/GameViewController.swift
@@ -7,24 +7,21 @@
 //
 
 import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var skview: SKView!
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-    */
-
+    
 }
