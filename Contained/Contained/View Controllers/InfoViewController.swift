a6680cf7cb63e4064fcac042c11032a2c21a82bc
dillonp23 <dillonp.lambda@gmail.com>
2019-08-07 18:38:11 -0700

    Changes to InfoViewControllers on Main.storyboard

--- Contained/Contained/View Controllers/InfoViewController.swift
+++ Contained/Contained/View Controllers/InfoViewController.swift
@@ -17,14 +17,8 @@ class InfoViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func doneTapped(_ sender: UIStoryboardSegue) {
+        navigationController?.popToRootViewController(animated: true)
     }
-    */
 
 }
