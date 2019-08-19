8340afc60b3d2e76a93ab346233fdc264e264c1e
AustinPotts <pottsevolvements@gmail.com>
2019-08-19 16:11:35 -0400

    Unwind Segue Complete

---
+++ ContainedApp/ContainedApp/View Controllers/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  ContainedApp
+//
+//  Created by Austin Potts on 8/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
