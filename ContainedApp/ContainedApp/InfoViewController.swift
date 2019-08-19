878d74e62b91c7463dbf6320d1ec943dbdc27f86
donellabarcelo <donellabarcelo95@gmail.com>
2019-08-19 15:40:17 -0700

    Completed project.

--- ContainedApp/ContainedApp/InfoViewController.swift
+++ ContainedApp/ContainedApp/InfoViewController.swift
@@ -9,14 +9,13 @@
 import UIKit
 
 class InfoViewController: UIViewController {
-
+    
+    @IBAction func unwindToInfoViewController (for unwindSegue: UIStoryboardSegue, towards subsequentVC: UIViewController) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func done(_ sender: Any) {
-        navigationController?.popToRootViewController(animated: true)
     }
 }
