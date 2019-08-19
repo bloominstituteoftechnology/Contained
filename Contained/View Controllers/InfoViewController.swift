bf08a6b80ae40b69d18c2bb2f9ef3e42c8636a27
Percy <percival.ngan@gmail.com>
2019-08-19 12:52:01 -0500

    added info screens

---
+++ Contained/View Controllers/InfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InfoViewController.swift
+//  Contained
+//
+//  Created by Percy Ngan on 8/19/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+	@IBAction func doneButtonPressed(_ sender: Any) {
+		navigationController?.popToRootViewController(animated: true)
+	}
+
+
+
+    // MARK: - Navigation
+
+
+
+}
