71ad7effc15c25b83f080b032da1cac568f770c9
Jerry Haaser <jerry.haaser@hotmail.com>
2019-08-19 15:37:39 -0500

    Added and hooked up zoom and roll buttons

--- ContainedFlex/SettingsViewController.swift
+++ ContainedFlex/SettingsViewController.swift
@@ -16,15 +16,21 @@ class SettingsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func rollToggle(_ sender: UISwitch) {
+        if sender.isOn {
+            Settings.shared.shouldRoll = true
+        } else {
+            Settings.shared.shouldRoll = false
+        }
     }
-    */
+    @IBAction func zoomToggle(_ sender: UISwitch) {
+        if sender.isOn {
+            Settings.shared.shouldZoom = true
+        } else {
+            Settings.shared.shouldZoom = false
+        }
+    }
+    
+
 
 }
