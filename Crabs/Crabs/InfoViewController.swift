15de551f53c2d1b57f086a9323b10271a619ddf7
William Chen <chen.will7@gmail.com>
2019-08-19 13:18:27 -0700

    buttons named and unwind completed

--- Crabs/Crabs/InfoViewController.swift
+++ Crabs/Crabs/InfoViewController.swift
@@ -10,10 +10,8 @@ import UIKit
 
 class InfoViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBAction func unwindToInfo(_ sender: UIStoryboardSegue){
+        
     }
     
 
