cf75da4188c5ef73adfc96e5b21a4d75e349e15e
Jonalynn Masters <jonalynna.masters@gmail.com>
2019-09-16 16:01:21 -0700

    Intermediate Commit: Pre-Game Screen Build

---
+++ Crabs/Crabs/View Controllers/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  Crabs
+//
+//  Created by Jonalynn Masters on 9/16/19.
+//  Copyright © 2019 Jonalynn Masters. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Crabs/Crabs/View Controllers/SettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsViewController.swift
+//  Crabs
+//
+//  Created by Jonalynn Masters on 9/16/19.
+//  Copyright © 2019 Jonalynn Masters. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    @IBAction func roll(_ sender: UISwitch) {
+    }
+    
+    @IBAction func zoom(_ sender: UISwitch) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+  
+
+}
