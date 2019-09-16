d87c4df2bd6c45e8b6ada4b7ecc8be5c4eab8ded
Isvvc <lyons@tuta.io>
2019-09-16 12:44:34 -0600

    Import assets. Add Info and Settings screens.

---
+++ Contained/Contained/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Isaac Lyons on 9/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+}
---
+++ Contained/Contained/SettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsViewController.swift
+//  Contained
+//
+//  Created by Isaac Lyons on 9/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        Settings.shared.shouldRoll = sender.isOn
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        Settings.shared.shouldZoom = sender.isOn
+    }
+    
+}
