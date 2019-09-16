1e3152436d351bdce446b95067492d971e99e84b
Jesse Ruiz <jesseleeruiz@Jesses-MacBook-Air.local>
2019-09-16 10:34:16 -1000

    Finished Setting Screen

--- CrabsContained/CrabsContained/SettingsViewController.swift
+++ CrabsContained/CrabsContained/SettingsViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        sender.isOn.toggle()
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        sender.isOn.toggle()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
