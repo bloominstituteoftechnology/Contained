54b93a0991aac16ff520879a0a7b4ad52f8a8d87
Jerry Haaser <jerry.haaser@hotmail.com>
2019-09-16 19:15:46 -0500

    fixed done button

--- Containedflexflex/Containedflexflex/SettingsViewController.swift
+++ Containedflexflex/Containedflexflex/SettingsViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import SpriteKit
 
 class SettingsViewController: UIViewController {
+    
+    let crab = SKSpriteNode()
+    
+    @IBOutlet weak var crabLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,16 +37,15 @@ class SettingsViewController: UIViewController {
             Settings.shared.shouldZoom = false
         }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func crabSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            crabLabel.text = "Waiting Crab"
+            crab.loadTextures(named: "WaitingCrab", forKey: SKSpriteNode.textureKey)
+        } else {
+            crabLabel.text = "Happy Crab"
+            crab.loadTextures(named: "HappyCrab", forKey: SKSpriteNode.textureKey)
+        }
     }
-    */
-
+    
+    
 }
