499e6b122cfcc1d0908f3aa338ac3d5975977677
Craig Swanson <cswanson02@me.com>
2019-10-01 23:15:01 -0500

    Created View Controllers and a tab view control

---
+++ Contained/Contained/GameViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GameViewController.swift
+//  Contained
+//
+//  Created by Craig Swanson on 10/1/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+ 
+
+}
---
+++ Contained/Contained/SettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.swift
+//  Contained
+//
+//  Created by Craig Swanson on 10/1/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+  
+}
