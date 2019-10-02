0ce9f60442f97831fa08dc2f7404946ef271f762
Craig Swanson <cswanson02@me.com>
2019-10-02 00:26:50 -0500

    Added Sprite Kit code

--- Contained/Contained/GameViewController.swift
+++ Contained/Contained/GameViewController.swift
@@ -12,6 +12,9 @@ import SpriteKit
 class GameViewController: UIViewController {
     @IBOutlet weak var skview: SKView!
     
- 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+    }
 }
