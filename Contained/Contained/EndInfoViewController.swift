72077cee15d23044826acd842c3c7cde900b76cd
Craig Swanson <cswanson02@me.com>
2019-10-01 23:38:31 -0500

    Added poptorootviewcontroller in the info track

---
+++ Contained/Contained/EndInfoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EndInfoViewController.swift
+//  Contained
+//
+//  Created by Craig Swanson on 10/1/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class EndInfoViewController: UIViewController {
+
+  @IBAction func done(_ sender: Any) {
+      navigationController?.popToRootViewController(animated: true)
+  }
+
+}
--- Contained/Contained/InfoViewController.swift
+++ Contained/Contained/InfoViewController.swift
@@ -10,5 +10,8 @@ import UIKit
 
 class InfoViewController: UIViewController {
 
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
 
 }
