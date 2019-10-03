93f9b0c0467cbca67bb413270fe3854b3cfd8159
Nicolas  Rios <nickrios33@yahoo.com>
2019-10-03 13:34:33 -0700

    added colors to view controller

--- Contained/Contained/GameViewController.swift
+++ Contained/Contained/GameViewController.swift
@@ -13,7 +13,7 @@ class GameViewController: UIViewController {
 
     @IBOutlet weak var skview: SKView!
 
-var skscene: CustomScene? = nil
+     var skscene: CustomScene? = nil
 
 override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
