0429642e8450f797a900041f9f2f35308d83f1c4
Nicolas  Rios <nickrios33@yahoo.com>
2019-10-02 18:19:55 -0700

    progress on project

---
+++ Contained/Contained/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Nicolas Rios on 10/2/19.
+//  Copyright © 2019 Nicolas Rios. All rights reserved.
+//
+
+
+import Foundation
+
+class Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+}
