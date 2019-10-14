108f6d4ce39ecddc14c564dcdc4ff2b7905bb380
Jon Bash <jonbash@protonmail.com>
2019-10-14 12:46:10 -0700

    set up settings

--- Crabs/Crabs/SettingsViewController.swift
+++ Crabs/Crabs/SettingsViewController.swift
@@ -9,22 +9,16 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    private(set) var roll: Bool = false
+    private(set) var zoom: Bool = false
+    
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        roll = sender.isOn
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        zoom = sender.isOn
     }
-    */
-
 }
