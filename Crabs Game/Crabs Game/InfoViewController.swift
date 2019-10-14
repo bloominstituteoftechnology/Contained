ba9f936c3222b644569e2c19152989dfc8660fed
Nar Kumar <njn.kumar@gmail.com>
2019-10-14 13:27:03 -0700

    Settings tab complete

---
+++ Crabs Game/Crabs Game/InfoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InfoViewController.swift
+//  Crabs Game
+//
+//  Created by Niranjan Kumar on 10/14/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func unwindToHome( _ sender: UIStoryboardSegue) {}
+
+}
