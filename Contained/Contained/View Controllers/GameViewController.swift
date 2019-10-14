0c6813f7865bc77ba22947e670c3bd99da2b1192
Dan M <djmthedev@gmail.com>
2019-10-14 13:13:58 -0700

    added functionality to GameViewController

--- Contained/Contained/View Controllers/GameViewController.swift
+++ Contained/Contained/View Controllers/GameViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
-
+    @IBOutlet weak var skView: SKView!
+    
+    var skscene: CustomScene? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skView.presentScene(skscene)
     }
     
 
