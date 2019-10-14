aa188796a9558cc809588f2375a744b2b56fc082
Nar Kumar <njn.kumar@gmail.com>
2019-10-14 13:01:47 -0700

    Initial Commit

---
+++ Crabs Game/Crabs Game/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  Crabs Game
+//
+//  Created by Niranjan Kumar on 10/14/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+}
