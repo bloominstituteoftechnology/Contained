3ed4bd37f80a4040c1cf53aee8ef41e79e977d5e
Jon Bash <jonbash@protonmail.com>
2019-10-14 12:30:37 -0700

    set up interface

---
+++ Crabs/Crabs/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  Crabs
+//
+//  Created by Jon Bash on 2019-10-14.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
