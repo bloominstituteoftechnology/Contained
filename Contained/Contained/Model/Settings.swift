f3c71722d0205c648c96c4b41397459dfba918ec
Chad Rutherford <chad_rutherford@outlook.com>
2019-11-11 14:54:49 -0500

    Added 'Settings.swift'

---
+++ Contained/Contained/Model/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Chad Rutherford on 11/11/19.
+//  Copyright © 2019 Chad Rutherford. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+}
