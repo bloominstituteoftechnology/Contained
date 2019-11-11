8c241659bdafcdb71cb2ef0aa08e92105aeea0b3
Tobi Tselios <tobi.tselios@gmail.com>
2019-11-11 22:41:16 +0000

    Build the Interface

---
+++ Crab/Crab/Models/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  Crab
+//
+//  Created by Tobi Kuyoro on 11/11/2019.
+//  Copyright © 2019 Tobi Kuyoro. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+}
