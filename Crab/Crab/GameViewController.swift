773f799896b3ee2c4d784df972af829d9de3a6f8
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2019-11-12 01:47:52 +0100

    finalized part 2

--- Crab/Crab/GameViewController.swift
+++ Crab/Crab/GameViewController.swift
@@ -8,34 +8,25 @@
 import SpriteKit
 import UIKit
 
-class GameViewController: UIViewController {
-
-    @IBOutlet weak var playground: SKView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+class GamesViewController: UIViewController {
+    @IBOutlet weak var skview: SKView!
     
-    class GamesViewController: UIViewController {
-        @IBOutlet weak var skview: SKView!
-        
-        var skscene: CustomScene? = nil
-        
-        override func viewDidAppear(_ animated: Bool) {
-            super.viewDidAppear(animated)
-            skscene = CustomScene(size: view.bounds.size)
-            skview.presentScene(skscene)
-        }
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+
