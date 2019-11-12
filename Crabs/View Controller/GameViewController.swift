cda8165d7cf713e2f1d81500e29c77ec43f5c949
Zack Larsen <zacklarsen1@gmail.com>
2019-11-11 17:09:19 -0700

    piecing it all together

---
+++ Crabs/View Controller/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  Crabs
+//
+//  Created by Zack Larsen on 11/11/19.
+//  Copyright © 2019 Zack Larsen. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GamesViewController: UIViewController {
+    
+
+    @IBOutlet weak var skview: SKView!
+    
+    var skscene: CustomScene? = nil
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+
+
+
+
+
+}
+}
--- Crabs/View Controller/InfoViewController.swift
+++ Crabs/View Controller/InfoViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class InfoViewController: UIViewController {
     
-}
+
 
 @IBAction func done(_ sender: Any) {
       navigationController?.popToRootViewController(animated: true)
   }
+}
