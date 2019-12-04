4fa0ba3089153eac2a67b6c9e6faea22b4d7dc51
Jeremy Taylor <jtaylor@jctaylor.org>
2019-12-04 12:24:12 -0600

    Added Settings model

---
+++ Contained/Contained/Models/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Jeremy Taylor on 12/4/19.
+//  Copyright © 2019 Bytes-Random L.L.C. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+}
