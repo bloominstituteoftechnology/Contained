354c51c3197ae2d0fa0c3e0ee60c0c6e1a303bfe
David Williams <burmesedragon@att.net>
2019-12-04 01:05:14 -0800

    View Controllers built and classes files generated

---
+++ CrabGame/CrabGame/Swift Files/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  CrabGame
+//
+//  Created by David Williams on 12/4/19.
+//  Copyright © 2019 david williams. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CrabGame/CrabGame/Swift Files/settings.swift
@@ -0,0 +1,17 @@
+//
+//  settings.swift
+//  CrabGame
+//
+//  Created by David Williams on 12/3/19.
+//  Copyright © 2019 david williams. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+}
