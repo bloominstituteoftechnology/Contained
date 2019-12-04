af71f096d3265a712015634f0286a816ed92c8f6
sal562 <salmanamer@gmail.com>
2019-12-04 03:36:00 -0800

    setup storyboards and viewcontrollers

---
+++ Contained/Contained/View Controllers/InfoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  InfoViewController.swift
+//  Contained
+//
+//  Created by LambdaSchoolVM_Catalina on 12/4/19.
+//  Copyright © 2019 Sal Amer. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ Contained/Contained/View Controllers/SettingsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.swift
+//  Contained
+//
+//  Created by LambdaSchoolVM_Catalina on 12/4/19.
+//  Copyright © 2019 Sal Amer. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
