ce42b7e15195a66542952ce4391e7c13eed52263
Joshua Rutkowski <jr.rtkski@gmail.com>
2019-12-04 18:13:06 -0500

    Finished setting up views and created controllers

---
+++ Contained/Contained/Controllers/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  Contained
+//
+//  Created by Joshua Rutkowski on 12/4/19.
+//  Copyright © 2019 Joshua Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
