f238db505ac033e6f9dd5161590116b5ccae3e3c
David <davidtwright89@gmail.com>
2019-12-04 00:57:08 -0500

    Created a new Xcode project named 'Contained'

---
+++ Contained/Contained/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contained
+//
+//  Created by David Wright on 12/4/19.
+//  Copyright © 2019 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
