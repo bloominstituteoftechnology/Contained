a3dc6b0b20c72f171a85883ffd9dfec8c881077e
morgan smith <mssmitty07@gmail.com>
2019-12-04 21:53:41 -0500

    initial commit

---
+++ Contained/Contained/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contained
+//
+//  Created by Morgan Smith on 12/4/19.
+//  Copyright © 2019 Morgan Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
