6472d33aa48a28d69adcf7caa65b90f3c8cc10f5
David <davidtwright89@gmail.com>
2019-12-04 22:50:42 -0500

    Finished building the Settings screen

--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    var rollIsOn: Bool = false
+    var zoomIsOn: Bool = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - IBActions
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        rollIsOn = sender.isOn
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        zoomIsOn = sender.isOn
     }
     
 
