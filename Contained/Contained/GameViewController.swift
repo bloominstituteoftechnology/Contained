3eea3bac3e2bcbbddd8a98c807d95add246fe1a1
morgan smith <mssmitty07@gmail.com>
2019-12-05 19:49:56 -0500

    final commit

---
+++ Contained/Contained/GameViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GameViewController.swift
+//  Contained
+//
+//  Created by Morgan Smith on 12/5/19.
+//  Copyright © 2019 Morgan Smith. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    @IBOutlet weak var skview: SKView!
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+    }
+    }
+
+
+
