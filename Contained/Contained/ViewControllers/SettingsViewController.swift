c96f35cfbd40aaf2d20fcd017062f02e68ed3d0d
Christopher Price <christopher.r.price@gmail.com>
2019-12-05 20:55:00 -0500

    Added everything except the stretch goals. Just short of MVP because the toggle switches don't work.

--- Contained/Contained/ViewControllers/SettingsViewController.swift
+++ Contained/Contained/ViewControllers/SettingsViewController.swift
@@ -17,18 +17,12 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func toggleRoll(_ sender: UISwitch) {
+        
     }
     
     @IBAction func toggleZoom(_ sender: UISwitch) {
+        
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
