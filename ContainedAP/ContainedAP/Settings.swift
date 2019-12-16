0a4c7ba1f63ee100621e70f32ce5daf5a7b21faf
Jorge Alvarez <jorgelalvarez52@gmail.com>
2019-12-16 12:38:10 -0800

    added interface

---
+++ ContainedAP/ContainedAP/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  ContainedAP
+//
+//  Created by Jorge Alvarez on 12/16/19.
+//  Copyright © 2019 Jorge Alvarez. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+}
