6e39d347ac62e1de1602979c9ff65f255917808f
Rohan Taylor <rhntaylor@gmail.com>
2019-12-16 15:15:49 -0800

    DONE

---
+++ Crabs/Crabs/InfoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  InfoViewController.swift
+//  Crabs
+//
+//  Created by Rohan Taylor on 12/16/19.
+//  Copyright © 2019 Rohan Taylor. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Crabs/Crabs/SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  Crabs
+//
+//  Created by Rohan Taylor on 12/16/19.
+//  Copyright © 2019 Rohan Taylor. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var toggleRoll: UISwitch!
+    @IBOutlet weak var toggleZoom: UISwitch!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
