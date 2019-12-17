e812d1c1c2bbc64cf682eff7acab555a510f5827
Aaron Cleveland <toscleveland@gmail.com>
2019-12-16 21:05:19 -0600

    Finished up to(Build the game screen)

---
+++ contained/contained/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  contained
+//
+//  Created by Aaron Cleveland on 12/16/19.
+//  Copyright © 2019 Aaron Cleveland. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ contained/contained/InfoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  InfoViewController.swift
+//  contained
+//
+//  Created by Aaron Cleveland on 12/16/19.
+//  Copyright © 2019 Aaron Cleveland. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
