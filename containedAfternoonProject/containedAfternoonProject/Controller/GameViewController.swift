4edbf8643c3f34efc2f5e12a4db4aa8d629d03d2
Stephanie Ballard <user.name>
2019-12-17 14:06:01 -0800

    changed button icons

--- containedAfternoonProject/containedAfternoonProject/Controller/GameViewController.swift
+++ containedAfternoonProject/containedAfternoonProject/Controller/GameViewController.swift
@@ -6,25 +6,32 @@
 //  Copyright © 2019 Stephanie Ballard. All rights reserved.
 //
 
-import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var skView: SKView!
+                
+    var skscene: CustomScene? = nil
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
     }
-    */
-
 }
+// Do any additional setup after loading the view.
+
+
+
+
+//clean code structure
+//import
+// enum
+//struct
+//classes
+//outlets
+//viewdid load
+//helper functions
+//actions
+//extensions
--- containedAfternoonProject/containedAfternoonProject/Model/Settings.swift
+++ containedAfternoonProject/containedAfternoonProject/Model/Settings.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2019 Stephanie Ballard. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+}
+
+
+
