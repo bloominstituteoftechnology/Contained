1788fcadf4383f257b37999d7b8641e3c7a177b9
Stephanie Ballard <user.name>
2019-12-16 16:10:31 -0800

    saving

---
+++ containedAfternoonProject/containedAfternoonProject/Model/Settings.swift
@@ -0,0 +1,9 @@
+//
+//  Settings.swift
+//  containedAfternoonProject
+//
+//  Created by Stephanie Ballard on 12/16/19.
+//  Copyright © 2019 Stephanie Ballard. All rights reserved.
+//
+
+import Foundation
