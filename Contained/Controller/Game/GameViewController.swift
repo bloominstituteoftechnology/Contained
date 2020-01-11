99c0ea98fcb6e73a79aac9be52f63d56c3644c18
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-01-10 16:13:16 -0800

    commit to track master repo

---
+++ Contained/Controller/Game/GameViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GameViewController.swift
+//  Contained
+//
+//  Created by Kenny on 12/15/19.
+//  Copyright © 2019 Hazy Studios. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    @IBOutlet weak var skview: SKView!
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+    }
+}
+
