eba3623a31559d0b5a6b3bfc48cfd05d6922300a
Keri Levesque <kerianne116@gmail.com>
2020-01-27 18:07:49 -0500

    fixing roll and zoom toggle

--- Crabs/Crabs/SettingsViewController.swift
+++ Crabs/Crabs/SettingsViewController.swift
@@ -10,25 +10,25 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var rollSwitch: UISwitch!
     
+    @IBOutlet weak var zoomSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+  }
+
     @IBAction func toggleRoll(_ sender: UISwitch) {
+        rollSwitch.setOn(sender.isOn, animated: true
+        )
     }
     
     @IBAction func toggleZoom(_ sender: UISwitch) {
+         zoomSwitch.setOn(sender.isOn, animated: true)
+        
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
