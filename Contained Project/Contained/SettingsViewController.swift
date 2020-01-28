fc1c09a0310b2ab35461a5474b86fdcf53611f01
Malik <malik21barnes@gmail.com>
2020-01-27 18:28:13 -0800

    Final commit

--- Contained Project/Contained/SettingsViewController.swift
+++ Contained Project/Contained/SettingsViewController.swift
@@ -17,11 +17,11 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func toggleRoll(_ sender: UISwitch) {
-        
+        Settings.shared.shouldRoll = sender.isOn
     }
     
     @IBAction func toggleZoom(_ sender: UISwitch) {
-        
+        Settings.shared.shouldZoom = sender.isOn
     }
     
     /*
