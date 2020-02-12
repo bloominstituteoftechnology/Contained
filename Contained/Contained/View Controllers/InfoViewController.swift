1798777c3a642f6d8bf3cae552a7dfa1cba24671
Christopher DeVito <toolofjesus@protonmail.com>
2020-02-12 17:26:07 -0500

    Built the info sequence.

---
+++ Contained/Contained/View Controllers/InfoViewController.swift
@@ -0,0 +1,28 @@
+//
+//  InfoViewController.swift
+//  Contained
+//
+//  Created by Lambda_School_Loaner_259 on 2/12/20.
+//  Copyright © 2020 DeVitoC. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    @IBAction func done(_ sender: UIStoryboardSegue) {
+        //navigationController?.popToRootViewController(animated: true)
+    }
+    
+    //@IBOutlet var label: UILabel! = UILabel(frame: CGRect(x: 0, y: 0, width: 160, height: 80))
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //label.text = "Crabs"
+        //label.textAlignment = .center
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
