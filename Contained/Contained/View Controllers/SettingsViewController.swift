6b136c6481a68cb71b8cb47476006ef40464fb80
Christopher DeVito <toolofjesus@protonmail.com>
2020-02-12 18:13:26 -0500

    Build the settings view controller.

--- Contained/Contained/View Controllers/SettingsViewController.swift
+++ Contained/Contained/View Controllers/SettingsViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    // MARK: IBActions
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        Settings.shared.shouldRoll = sender.isOn
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        Settings.shared.shouldZoom = sender.isOn
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +26,4 @@ class SettingsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
