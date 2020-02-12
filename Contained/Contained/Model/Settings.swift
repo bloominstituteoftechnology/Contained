23087d81b1a7c664a8a762a071078403e0b8c075
Claudia Maciel <clc_80@yahoo.com>
2020-02-12 15:23:39 -0800

    Created settings model

---
+++ Contained/Contained/Model/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Claudia Contreras on 2/12/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoon = false
+}
