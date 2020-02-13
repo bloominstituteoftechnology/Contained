14ec85122a7eb3f106f84c75b145614f65f4e455
Breena <breena_greek@live.com>
2020-02-13 13:46:05 -0500

    Updated Fade Function

--- HappyCrab/HappyCrab/Models/CustomScene.swift
+++ HappyCrab/HappyCrab/Models/CustomScene.swift
@@ -36,7 +36,9 @@ class CustomScene: SKScene {
         let rollAction = SKAction.rotate(byAngle: CGFloat.pi * 2, duration: actionDuration)
         let zoomAction = SKAction.scale(by: 1.3, duration: 0.3)
         let unzoomAction = SKAction.scale(to: 1.0, duration: 0.1)
-        let fadeAction = SKAction.fadeAlpha(to: 0.5, duration: 0.3)
+        let fadeInAction = SKAction.fadeIn(withDuration: 0.3)
+        let fadeOutAction = SKAction.fadeOut(withDuration: 0.3)
+        let fadeActionSequence = SKAction.sequence([fadeInAction, fadeOutAction, fadeInAction])
         
         switch Settings.shared.shouldZoom {
         case false:
@@ -50,7 +52,7 @@ class CustomScene: SKScene {
             crab.run(rollAction)
         }
         if Settings.shared.shouldFade {
-            crab.run(fadeAction)
+            crab.run(fadeActionSequence)
         }
     }
 }
