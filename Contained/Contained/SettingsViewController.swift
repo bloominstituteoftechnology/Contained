515acbbbf909a610c47c77610a437ba58530ae78
kensam1 <samarakodykennedy@gmail.com>
2020-02-13 00:21:52 -0500

    Krab Game

---
+++ Contained/Contained/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  Contained
+//
+//  Created by Lambda_School_Loaner_241 on 2/12/20.
+//  Copyright © 2020 Lambda_School_Loaner_241. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        Settings.shared.shouldRoll = sender.isOn
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        Settings.shared.shouldZoom = sender.isOn  // singleton property
+       
+
+    }
+    
+
+}
