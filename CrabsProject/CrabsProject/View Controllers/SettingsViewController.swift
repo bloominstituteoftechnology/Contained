2c0d947cb4dfe755cf2247c0d9913e72831bfabd
Jarren Campos <jarrenmcampos@gmail.com>
2020-02-14 19:27:57 -0800

    Fixed settings

--- CrabsProject/CrabsProject/View Controllers/SettingsViewController.swift
+++ CrabsProject/CrabsProject/View Controllers/SettingsViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
 
-    @IBOutlet var toggleZoom: UISwitch!
-    @IBOutlet var toggleRoll: UISwitch!
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        Settings.shared.shouldRoll = sender.isOn
+    }
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        Settings.shared.shouldZoom = sender.isOn
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,14 +25,5 @@ class SettingsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
