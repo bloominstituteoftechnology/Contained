c00b2824159f4216a15d44ed51e28110f02e6b56
Hector L <hectoremil21@hotmail.com>
2020-02-22 19:33:50 -0500

    Built Info Sequence

---
+++ CrabGame/CrabGame/Controllers/GameViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GameViewController.swift
+//  CrabGame
+//
+//  Created by Karen Rodriguez on 2/22/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+}
---
+++ CrabGame/CrabGame/Controllers/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoViewController.swift
+//  CrabGame
+//
+//  Created by Karen Rodriguez on 2/22/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
+    
+}
