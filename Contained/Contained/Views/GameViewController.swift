b7e865da25d6e9379c1b55aa78589a8fcb257171
Samuel Esserman <samessermanlambda@gmail.com>
2020-02-24 14:59:31 -0500

    Still having errors, complete up to stretch goals

---
+++ Contained/Contained/Views/GameViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GameViewController.swift
+//  Contained
+//
+//  Created by Samuel Esserman on 2/24/20.
+//  Copyright © 2020 Samuel Esserman. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var skscene: SKView!
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+    }
+
+}
---
+++ Contained/Contained/Views/SettingsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.swift
+//  Contained
+//
+//  Created by Samuel Esserman on 2/24/20.
+//  Copyright © 2020 Samuel Esserman. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    let settings = Settings.shared
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        settings.shouldRoll = sender.isOn
+    }
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        settings.shouldZoom = sender.isOn
+    }
+}
