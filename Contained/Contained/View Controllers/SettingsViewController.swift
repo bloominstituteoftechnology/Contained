a92c051cbd14c0f00c30500c1a2ab8ef9d561fae
Harmony Radley <harmonyradley99@gmail.com>
2020-02-24 17:06:27 -0500

    Creating Build the Game Screen

--- Contained/Contained/View Controllers/SettingsViewController.swift
+++ Contained/Contained/View Controllers/SettingsViewController.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2020 Harmony Radley. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class SettingsViewController: UIViewController {
+
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+    }
+    
+    @IBAction func toggleHappyCrab(_ sender: UISwitch) {
+    }
+    
+}
