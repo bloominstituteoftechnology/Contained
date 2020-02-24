8f81ca3234e6d26c1ee42abcfc61ae2c33cbac58
Harmony Radley <harmonyradley99@gmail.com>
2020-02-24 10:55:47 -0500

    Started Contained Project

---
+++ Contained/Contained/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Harmony Radley on 2/24/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+}
+
