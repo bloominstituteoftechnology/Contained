0d96577f81b5fc7b8e4b60a1be33767cddebdb76
Samuel Esserman <samessermanlambda@gmail.com>
2020-02-24 14:00:18 -0500

    First commit, havent started

---
+++ Contained/Contained/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contained
+//
+//  Created by Samuel Esserman on 2/24/20.
+//  Copyright © 2020 Samuel Esserman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
