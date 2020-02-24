df20f47861e80c82e613cfe1924e03c38105c32d
Shawn Gee <shawngee@gmail.com>
2020-02-24 11:40:29 -0500

    Fixed Settings

--- CrabGame/CrabGame/SettingsViewController.swift
+++ CrabGame/CrabGame/SettingsViewController.swift
@@ -22,6 +22,7 @@ class SettingsViewController: UIViewController {
     }
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
