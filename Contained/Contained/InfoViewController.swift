48633b54d2cada7051f0d9109d5825946e3da729
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-02-24 18:17:56 -0800

    Created InfoView and Setting files

---
+++ Contained/Contained/InfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoViewController.swift
+//  Contained
+//
+//  Created by Bhawnish Kumar on 2/24/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
