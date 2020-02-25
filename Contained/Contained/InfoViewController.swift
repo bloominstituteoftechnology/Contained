4bbcfe46c3e381c57baad40040b3d05728a4a11b
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-02-24 18:39:32 -0800

    Added view controllers to thhe info storyboard and labels

--- Contained/Contained/InfoViewController.swift
+++ Contained/Contained/InfoViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class InfoViewController: UIViewController {
 
+    @IBAction func done(_ sender: Any) {
+    navigationController?.popToRootViewController(animated: true)
+
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
