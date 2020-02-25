0002f607998f46712aac3a4068d45e5e2f51013e
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-02-24 19:29:12 -0800

    Created CustomView file and added code to it

--- Contained/Contained/GameViewController.swift
+++ Contained/Contained/GameViewController.swift
@@ -11,6 +11,17 @@ import UIKit
 class GameViewController: UIViewController {
 
     @IBOutlet weak var skview: SKView!
+    
+    var skscene: CustomScene? = nil
+        
+        override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            skscene = CustomScene(size: view.bounds.size)
+            skview.presentScene(skscene)
+        }
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
