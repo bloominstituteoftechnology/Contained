527426ac35268ae412aa88c9eedd0bb2872c859f
Hunter Oppel <hun.oppel@gmail.com>
2020-03-23 16:29:24 -0400

    Finished game view and MVP.

--- movingCrabGame/movingCrabGame/GameViewController.swift
+++ movingCrabGame/movingCrabGame/GameViewController.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
     
+    @IBOutlet weak var gameView: SKView!
     
+    var skscene: CustomScene? = nil
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        gameView.presentScene(skscene)
+    }
 }
