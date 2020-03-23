a648f6a195d9d395dac1d1b147bbeddb5da2bd3f
Hunter Oppel <hun.oppel@gmail.com>
2020-03-23 16:23:12 -0400

    Finished up the settings page.

--- movingCrabGame/movingCrabGame/SettingsViewController.swift
+++ movingCrabGame/movingCrabGame/SettingsViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        Settings.shared.shouldRoll = sender.isOn
+    }
     
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        Settings.shared.shouldZoom = sender.isOn
+    }
     
 }
