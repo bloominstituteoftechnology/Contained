9eebee5cdc57b4912b183abceb0c8485973f81b3
Aaron Peterson <aarondp2011@gmail.com>
2020-04-22 22:40:32 -0500

    Final commit

--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class SettingsViewController: UIViewController {
     
     @IBAction func toggleRoll(_ sender: UISwitch) {
+        Settings.shared.shouldRoll = true
     }
     
     @IBAction func toggleZoom(_ sender: UISwitch) {
+        Settings.shared.shouldZoom = true
     }
     
     
