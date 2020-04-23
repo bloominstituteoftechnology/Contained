cf575423035f5a659cd7132c454c7dcec19d87a9
Bronson <bronsonmullens@icloud.com>
2020-04-23 00:20:26 -0700

    Working on file tasks

---
+++ Contained/Contained/GameViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GameViewController.swift
+//  Contained
+//
+//  Created by Bronson Mullens on 4/22/20.
+//  Copyright © 2020 Bronson Mullens. All rights reserved.
+//
+
+import SpriteKit
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    class GamesViewController: UIViewController {
+        @IBOutlet weak var skview: SKView!
+        
+        var skscene: CustomScene? = nil
+        
+        override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            skscene = CustomScene(size: view.bounds.size)
+            skview.presentScene(skscene)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
