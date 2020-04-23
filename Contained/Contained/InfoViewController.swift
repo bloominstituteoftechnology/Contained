d86514c042ac7d219a5702164b24282fcd0874fb
Aaron Peterson <aarondp2011@gmail.com>
2020-04-22 21:50:59 -0500

    Set up Game VC

---
+++ Contained/Contained/InfoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InfoViewController.swift
+//  Contained
+//
+//  Created by Aaron Peterson on 4/22/20.
+//  Copyright © 2020 Aaron Peterson. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ Contained/Contained/SettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsViewController.swift
+//  Contained
+//
+//  Created by Aaron Peterson on 4/22/20.
+//  Copyright © 2020 Aaron Peterson. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
