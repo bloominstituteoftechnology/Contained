69cd4ea11c3ccef47d8bebf1098d2514bcb40cc3
Ian French <igf2011@hotmail.com>
2020-04-23 20:58:27 -0400

    Nearly complete, some errors

---
+++ HappyCrab/HappyCrab/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoViewController.swift
+//  HappyCrab
+//
+//  Created by Ian French on 4/22/20.
+//  Copyright © 2020 Ian French. All rights reserved.
+//
+
+
+import UIKit
+
+
+class InfoViewController: UIViewController {
+    
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+}
