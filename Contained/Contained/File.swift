0b2e93e16a6356dd2690d3f7ac6fcb04b339727d
Rich Gibbs <rich-gibbs@lambdastudents.com>
2020-05-18 13:27:36 -0700

    added a settings file

---
+++ Contained/Contained/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Richard Gibbs on 5/18/20.
+//
+
+import Foundation
---
+++ Contained/Contained/Resources/Settings.swift
@@ -0,0 +1,9 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Richard Gibbs on 5/18/20.
+//  Copyright © 2020 Rich Gibbs. All rights reserved.
+//
+
+import Foundation
