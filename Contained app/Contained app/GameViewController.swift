18af9621d9fe46e3c1d3bd4b4ae0501895474109
dmills14 <drew-miller@lambdastudents.com>
2020-05-18 13:19:57 -0700

    Added SpriteKit

--- Contained app/Contained app/GameViewController.swift
+++ Contained app/Contained app/GameViewController.swift
@@ -6,15 +6,22 @@
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
-import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var skview: SKView!
+   
+    var skscene: CustomScene? = nil
+        
+        override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            skscene = CustomScene(size: view.bounds.size)
+            skview.presentScene(skscene)
+        }
+    }
 
         // Do any additional setup after loading the view.
-    }
     
 
     /*
@@ -27,4 +34,4 @@ class GameViewController: UIViewController {
     }
     */
 
-}
+
