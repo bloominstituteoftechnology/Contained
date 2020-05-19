d06010a614ec7b2c5c036e39ff525a639bd9359b
Mike Nichols <mike@nixco.net>
2020-05-19 14:55:35 -0500

    Thanks for the help Michael!

--- Contained-game/Contained-game/Models/GameViewController.swift
+++ Contained-game/Contained-game/Models/GameViewController.swift
@@ -9,14 +9,15 @@
 import UIKit
 import SpriteKit
 
-class GamesViewController: UIViewController {
-    @IBOutlet weak var skview: SKView!
+class GameViewController: UIViewController {
+    
+    @IBOutlet weak var mySKView: SKView!
     
     var skscene: CustomScene? = nil
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         skscene = CustomScene(size: view.bounds.size)
-        skview.presentScene(skscene)
+        mySKView.presentScene(skscene)
     }
 }
