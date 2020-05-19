fae66f54970cf7090f796b9e27b86aac201c3af2
Bryan Cress <8675309+cressbj@users.noreply.github.com>
2020-05-18 21:49:45 -0400

    Game Screen Build Complete, MVP met

--- Contained/Contained/View Controllers/InfoViewController.swift
+++ Contained/Contained/View Controllers/InfoViewController.swift
@@ -12,20 +12,8 @@ class InfoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
     }
+    
+    
     @IBAction func unwindToInfoVC (_ sender: UIStoryboardSegue) {}
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Contained/Contained/View Controllers/SettingsViewController.swift
+++ Contained/Contained/View Controllers/SettingsViewController.swift
@@ -12,8 +12,6 @@ class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func toggleRoll(_ sender: UISwitch) {
@@ -33,18 +31,4 @@ class SettingsViewController: UIViewController {
             Settings.shared.shouldZoom = false
         }
     }
-    
-    
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
