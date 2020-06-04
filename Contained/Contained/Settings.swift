b068d4f2cb1184ea346aeb1634cfa1171f6002c3
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-06-03 20:37:20 -0700

    Completed project

--- Contained/Contained/Settings.swift
+++ Contained/Contained/Settings.swift
@@ -8,10 +8,14 @@
 
 import Foundation
 
+import SpriteKit
+
 class Settings {
     static let shared = Settings()
     private init() {}
     
     var shouldRoll = false
     var shouldZoom = false
+    var texture = "HappyCrab"
+    var lastPosition: CGPoint? = nil
 }
--- Contained/Contained/ViewControllers/SettingsViewController.swift
+++ Contained/Contained/ViewControllers/SettingsViewController.swift
@@ -13,18 +13,32 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        if Settings.shared.shouldRoll == false {
+            Settings.shared.shouldRoll = true
+        } else {
+            Settings.shared.shouldRoll = false
+        }
     }
-    */
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        if Settings.shared.shouldZoom == false {
+            Settings.shared.shouldZoom = true
+        } else {
+            Settings.shared.shouldZoom = false
+        }
+    }
 
+    @IBOutlet weak var toggleTexture: UISegmentedControl!
+    @IBAction func toggleTexture(_ sender: UISegmentedControl) {
+        switch toggleTexture.selectedSegmentIndex {
+        case 1:
+            Settings.shared.texture = "WaitingCrab"
+        default:
+            Settings.shared.texture = "HappyCrab"
+        }
+    }
+    
 }
