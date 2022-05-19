3f9edb4a7ea57eb883e28963a0999f781cd60147
Waseem Idelbi <idelbiw@gmail.com>
2022-05-18 20:27:40 -0400

    programmed the done button in the last view controller in the info storyboard pop to rootVC.

--- Contained/Contained/InfoViewController.swift
+++ Contained/Contained/InfoViewController.swift
@@ -9,21 +9,9 @@ import UIKit
 
 class InfoViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+//MARK: - IBActions
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
