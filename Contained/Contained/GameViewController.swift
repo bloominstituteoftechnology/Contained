08bfb9384c83013a2c9803691f42a44bac0b4ee4
Waseem Idelbi <idelbiw@gmail.com>
2022-05-18 21:11:32 -0400

    copied everything over for the GameVC, and created the file for CustomScene and copied over all the code for that

--- Contained/Contained/GameViewController.swift
+++ Contained/Contained/GameViewController.swift
@@ -6,24 +6,21 @@
 //
 
 import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
+    
+//MARK: - Properties
+    var skscene: CustomScene? = nil
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+//MARK: - IBOutlets
+    @IBOutlet weak var skView: SKView!
+    
+//MARK: - Methods
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skView.presentScene(skscene)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
